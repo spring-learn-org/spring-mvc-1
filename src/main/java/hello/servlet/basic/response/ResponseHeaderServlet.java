a8bfc0a637d0610a0a1aaae244087946594285db
hakjin <hakjin.kim@streami.co>
2024-06-17 08:08:02 +0900

    [Java-Spring - 스프링 MVC 1편] Http Servlet Response - Basic

---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "ResponseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // [status-line]
+        resp.setStatus(HttpServletResponse.SC_OK);
+
+        // [response-headers]
+        resp.setHeader("Content-Type", "text/plain;charset=utf-8");
+        resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        resp.setHeader("Pragma", "no-cache");
+        resp.setHeader("my-header", "hello");
+
+        resp.getWriter().write("ok");
+    }
+}
