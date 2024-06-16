bfd9e421922a4a4d2eba3dac5ea4acabb6c1467d
hakjin <hakjin.kim@streami.co>
2024-06-17 08:08:28 +0900

    [Java-Spring - 스프링 MVC 1편] Http Servlet Response - Basic - setContentType

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -16,11 +16,22 @@ public class ResponseHeaderServlet extends HttpServlet {
         resp.setStatus(HttpServletResponse.SC_OK);
 
         // [response-headers]
-        resp.setHeader("Content-Type", "text/plain;charset=utf-8");
         resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
         resp.setHeader("Pragma", "no-cache");
         resp.setHeader("my-header", "hello");
 
+        // [Header 편의 메서드]
+        content(resp);
+
         resp.getWriter().write("ok");
+    }
+
+    private void content(HttpServletResponse response) {
+        // Content-Type: text/plain;charset=utf-8
+        // Content-Length: 2
+        // response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        // response.setContentLength(2); // (생략시 자동 생성)
     }
 }
